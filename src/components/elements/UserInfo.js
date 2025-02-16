4abfb6d59175b8613a1bc80fd6b2d6f7b233ae11
hoang1805 <dhhoang203@gmail.com>
2025-02-17 00:23:19 +0700

    user info, logout

--- src/components/elements/UserInfo.js
+++ src/components/elements/UserInfo.js
@@ -2,13 +2,53 @@ import React, { useContext } from 'react';
 import AuthContext from '../../context/AuthContext';
 import '../../styles/components/elements/user.info.scss';
 import { useNavigate } from 'react-router-dom';
+import { Avatar, Dropdown } from 'antd';
+import api from '../../utils/Axios';
+import loading from '../../utils/Loading';
+import flash from '../../utils/Flash';
+import Client from '../../utils/client.manager';
+import { UserOutlined } from '@ant-design/icons';
 
 const UserInfo = () => {
-    const {user} = useContext(AuthContext);
+    const { user, logout } = useContext(AuthContext);
     const navigate = useNavigate();
+    const menu = [
+        {
+            key: '1',
+            label: <div onClick={() => navigate('/me')}>Thông tin cá nhân</div>,
+        },
+        {
+            key: '2',
+            label: (
+                <div
+                    onClick={() => {
+                        (async () => {
+                            try {
+                                loading.show();
+                                await api.post('/api/logout');
+                                Client.clear();
+                                logout();
+                                flash.success('Đăng xuất thành công');
+                                navigate('/login');
+                            } catch (err) {
+                                flash.error('Đăng xuất thất bại');
+                                console.error(err);
+                            } finally {
+                                loading.hide();
+                            }
+                        })();
+                    }}
+                >
+                    Đăng xuất
+                </div>
+            ),
+        },
+    ];
     return (
-        <div className='user-info' onClick={() => navigate('/me')}>
-            {user?.avatar ? '' : <img src='/avatar-default.svg' className='avatar' alt='avatar'/>}
+        <div className="user-info">
+            <Dropdown arrow={false} placement={'bottomRight'} menu={{ items: menu }}>
+                <Avatar size="large" icon={<UserOutlined />} />
+            </Dropdown>
         </div>
     );
 };
